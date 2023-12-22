0b97696cf77b94390a8f09c32370e007b22de5c4
Yuanqing <yl11249@nyu.edu>
2023-12-23 00:51:48 +0800

    1.2 button selection

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,14 @@
+let displayColorImage = false;
+
 class Figure {
-  constructor(name, imagePath, naturalColorPath, voicePrompt) {
+  constructor(name, bwimagePath, imagePath, naturalColor, voicePrompt) {
     this.name = name;
-    this.image = loadImage(imagePath); // Load the black and white image
-    this.naturalColorImage = loadImage(naturalColorPath); // Load the image for the natural color
+    this.bwimage = loadImage(bwimagePath); // Load the black and white image
+    this.image = loadImage(imagePath);
+    this.naturalColor = naturalColor;
     this.voicePrompt = voicePrompt; // The voice prompt text
   }
 
-  // You can add a method to speak the prompt if needed
   speakPrompt() {
     mSpeech.speak("What is the color of " + this.voicePrompt + "?");
   }
@@ -18,24 +20,26 @@ let buses = [];
 let mSpeech;
 
 function setup() {
-  createCanvas(800, 600); // Create a canvas of the desired size
+  createCanvas(windowWidth,windowHeight); // Create a canvas of the desired size
 
   // Initialize the speech object
   mSpeech = new p5.Speech();
   mSpeech.interrupt = false;
 
   // Initialize figures
-  figures.push(new Figure("Tomato", "bwtomato.png", "tomato.png", "tomato"));
+  figures.push(new Figure("Tomato", "bwtomato.png", "tomato.png",'red', "tomato"));
+  figures.push(new Figure("Pepper", "bwpepper.png", "greenpepper.png",'green', "pepper"));
+  figures.push(new Figure("Moon", "bwmoon.png", "moon.png",'yellow', "moon"));
+  figures.push(new Figure("Chestnut", "bwchestnut.png", "chestnut.png",'brown', "chestnut"));
 
-  // Example usage
-  figures[0].speakPrompt(); // This will speak the prompt for the first figure
+  
 
   // Initialize buses
   buses = [
-    { color: "green", position: { x: 100, y: 300 } },
-    { color: "red", position: { x: 200, y: 300 } },
-    { color: "yellow", position: { x: 300, y: 300 } },
-    { color: "brown", position: { x: 400, y: 300 } },
+    { color: "green" },
+    { color: "red" },
+    { color: "yellow" },
+    { color: "brown" },
   ];
 
   // Start the game
@@ -48,11 +52,17 @@ function sayPrompt(word) {
 }
 
 function draw() {
-  background(255); // Set background color to white
+  background(255); 
 
   // Draw the selected figure
   if (selectedFigure) {
-    image(selectedFigure.image, 150, 100, 100, 100); // Adjust position and size as needed
+    if (displayColorImage) {
+      // Display the color image
+      image(selectedFigure.image, 150, 150, 500, 500);
+    } else {
+      // Display the black and white image
+      image(selectedFigure.bwimage, 150, 150, 500, 500);
+    }
   }
 
   // Draw buses
@@ -60,11 +70,10 @@ function draw() {
 }
 
 function startGame() {
-  // Randomly select a figure
+  displayColorImage = false;
   selectedFigure = selectRandomFigure(figures);
+  selectedFigure.speakPrompt(figures); 
 
-  // Play voice prompt for the selected figure
-  selectedFigure.voicePrompt.play();
 }
 
 function selectRandomFigure(figures) {
@@ -73,8 +82,64 @@ function selectRandomFigure(figures) {
 }
 
 function drawBuses() {
-  buses.forEach((bus) => {
+  const busWidth = 180;
+  const busHeight = 100;
+  const gap = 30; 
+
+  // Calculate total height needed for all buses and gaps
+  const totalHeight = buses.length * busHeight + (buses.length - 1) * gap;
+
+  // Starting Y position to center the buses vertically
+  const startY = (windowHeight - totalHeight) / 2;
+
+  buses.forEach((bus, index) => {
+    // Calculate the Y position of each bus
+    const yPos = startY + index * (busHeight + gap);
+
     fill(bus.color);
-    rect(bus.position.x, bus.position.y, 50, 20); // Draw bus as a rectangle
+    // X position is set to keep the bus inside the canvas, adjust as needed
+    const xPos = windowWidth - busWidth - 80; 
+    rect(xPos, yPos, busWidth, busHeight);
   });
+}
+
+function keyPressed() {
+  if (key === 'g') {
+      checkSelection('green');
+  } else if (key === 'r') {
+      checkSelection('red');
+  } else if (key === 'y') {
+      checkSelection('yellow');
+  } else if (key === 'b') {
+      checkSelection('brown');
+  }
+}
+
+// Check if the selection matches the figure's color
+function checkSelection(selectedColor) {
+  if (selectedColor === selectedFigure.naturalColor) {
+      handleCorrectSelection();
+  } else {
+      handleIncorrectSelection();
+  }
+}
+
+// Handle correct selection
+function handleCorrectSelection() {
+  // Display the figure in its natural color
+  displayColorImage = true;
+  mSpeech.speak("Fantastic job! You got it right!");
+  // Implement the logic to display the figure in its natural color
+  console.log("Fantastic job! You got it right!");
+  // Proceed to the next figure after a brief pause
+  setTimeout(startGame, 3000); // Adjust the delay as needed
+}
+
+// Handle incorrect selection
+function handleIncorrectSelection() {
+  // Prompt the child to try again or show a hint
+  displayColorImage = false;
+  mSpeech.speak("Let's try one more time!");
+  // Implement the logic for incorrect selection
+  console.log("Incorrect selection. Let's try one more time!");
 }
